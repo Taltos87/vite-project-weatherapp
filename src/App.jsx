5b36a38dbd73c6bf9677a36d739b53af659c4391
Akelarre Taltos <93266473+Taltos87@users.noreply.github.com>
2023-06-04 19:25:25 +0100

    Package Management & Libraries

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+     <h1>Shecodes Weather App</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,18 +17,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+     
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+      <a href={`https://github.com/Taltos87/my-weather-app`} >Open Source Code</a> 
+        <p> by Ana-Maria Paraschivu </p>
+
+      </div>
     </>
   )
 }
